5b08d181f54486a27ab5c841ff73eed37d8af1a7
Niels Dekker <N.Dekker@lumc.nl>
2018-11-05 10:50:04 +0100

    STYLE: Added missing 'const' and 'static' to LoggerImpl member functions.

--- Modules/Logger/detail/selxLoggerImpl.h
+++ Modules/Logger/detail/selxLoggerImpl.h
@@ -70,7 +70,7 @@ public:
   // Stream std:vector to string
   // TODO: Use std::copy_n to print [n1, n2, ... , n-1, n] if vector is long
   template < typename T >
-  std::string operator<<( const std::vector< T >& v ) {
+  std::string operator<<( const std::vector< T >& v ) const {
     std::ostringstream out;
     if( !v.empty() ) {
       if( v.size() > 1 ) out << '[';
@@ -83,7 +83,7 @@ public:
 
   // Stream std::map< T, T > to string
   template < typename T >
-  std::string operator<<( const std::map< T, T >& m ) {
+  std::string operator<<( const std::map< T, T >& m ) const {
     std::ostringstream out;
     if( !m.empty() ) {
       out << "{";
@@ -99,7 +99,7 @@ public:
 
   // Stream std::map< T, std::vector< T > > to string
   template < typename T >
-  std::string operator<<( const std::map< T, std::vector< T > >& m ) {
+  std::string operator<<( const std::map< T, std::vector< T > >& m ) const {
     std::ostringstream out;
     if( !m.empty() ) {
       out << "{";
@@ -114,7 +114,7 @@ public:
   }
 
 private:
-  void RemoveTrailingCharsFromOutputStringStream(
+  static void RemoveTrailingCharsFromOutputStringStream(
     std::ostringstream& outputStringStream,
     unsigned numberOfCharsToRemove)
   {
